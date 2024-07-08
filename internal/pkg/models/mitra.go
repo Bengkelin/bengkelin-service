46bfb56b5da17fd311703eecd3b16dcd158d7fef
Muh Zaqi <zaqiikhwan22@gmail.com>
2024-07-08 17:51:27 +0700

    feat: mitra models

--- internal/pkg/models/mitra.go
+++ internal/pkg/models/mitra.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Mitra struct {
-	ID         string         `gorm:"primary_key;type:varchar(36)" json:"mitra_id"`
-	FirstName  string         `gorm:"type:varchar(255)" json:"first_name"`
-	LastName   string         `gorm:"type:varchar(255)" json:"last_name"`
-	Email      string         `gorm:"type:varchar(255);unique" json:"email"`
-	Password   string         `gorm:"type:varchar(255)" json:"password"`
-	NoTelp     string         `gorm:"type:varchar(255)" json:"no_telp"`
-	BankName   string         `gorm:"type:varchar(255)" json:"bank_name"`
-	BankNumber string         `gorm:"type:varchar(255)" json:"bank_number"`
-	CreatedAt  time.Time      `json:"created_at"`
-	UpdatedAt  time.Time      `json:"updated_at"`
-	DeletedAt  gorm.DeletedAt `gorm:"index;column:deleted_at" json:"deleted_at"`
+	ID          string         `gorm:"primary_key;type:varchar(36)" json:"mitra_id"`
+	FirstName   string         `gorm:"type:varchar(255)" json:"first_name"`
+	LastName    string         `gorm:"type:varchar(255)" json:"last_name"`
+	Email       string         `gorm:"type:varchar(255);unique" json:"email"`
+	Password    string         `gorm:"type:varchar(255)" json:"password"`
+	PhoneNumber string         `gorm:"type:varchar(255)" json:"phone_number"`
+	BankName    string         `gorm:"type:varchar(255)" json:"bank_name"`
+	BankNumber  string         `gorm:"type:varchar(255)" json:"bank_number"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index;column:deleted_at" json:"deleted_at"`
 }
