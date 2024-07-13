abec88f334dc2219be0172f8a51f10cfd13a369a
Muh Zaqi <zaqiikhwan22@gmail.com>
2024-07-13 14:45:10 +0700

    refactor: users model add relation addresses and phone_number field

--- internal/pkg/models/user.go
+++ internal/pkg/models/user.go
@@ -8,12 +8,14 @@ import (
 
 // User model
 type User struct {
-	ID        string         `gorm:"primary_key;type:varchar(36)" json:"user_id"`
-	FirstName string         `gorm:"type:varchar(255)" json:"first_name"`
-	LastName  string         `gorm:"type:varchar(255)" json:"last_name"`
-	Email     string         `gorm:"type:varchar(255);unique" json:"email"`
-	Password  string         `gorm:"type:varchar(255)" json:"password"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index;column:deleted_at" json:"deleted_at"`
+	ID          string         `gorm:"primary_key;type:varchar(36)" json:"user_id"`
+	FirstName   string         `gorm:"type:varchar(255)" json:"first_name"`
+	LastName    string         `gorm:"type:varchar(255)" json:"last_name"`
+	Email       string         `gorm:"type:varchar(255);unique" json:"email"`
+	PhoneNumber string         `gorm:"type:varchar(255)" json:"phone_number"`
+	Password    string         `gorm:"type:varchar(255)" json:"password"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index;column:deleted_at" json:"deleted_at"`
+	Addresses   []AddressUser  `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"addresses"`
 }
