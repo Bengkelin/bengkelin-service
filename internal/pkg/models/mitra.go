13f1ecff1084a3fc7b13f32ceb0d53c50f7d8438
Muh Zaqi <zaqiikhwan22@gmail.com>
2024-07-27 17:33:41 +0700

    adjust: add has many bengkels relation on mitra model

--- internal/pkg/models/mitra.go
+++ internal/pkg/models/mitra.go
@@ -11,11 +11,12 @@ type Mitra struct {
 	FirstName   string         `gorm:"type:varchar(255)" json:"first_name"`
 	LastName    string         `gorm:"type:varchar(255)" json:"last_name"`
 	Email       string         `gorm:"type:varchar(255);unique" json:"email"`
-	Password    string         `gorm:"type:varchar(255)" json:"password"`
+	Password    string         `gorm:"type:varchar(255)" json:"-"`
 	PhoneNumber string         `gorm:"type:varchar(255)" json:"phone_number"`
 	BankName    string         `gorm:"type:varchar(255)" json:"bank_name"`
 	BankNumber  string         `gorm:"type:varchar(255)" json:"bank_number"`
 	CreatedAt   time.Time      `json:"created_at"`
 	UpdatedAt   time.Time      `json:"updated_at"`
 	DeletedAt   gorm.DeletedAt `gorm:"index;column:deleted_at" json:"deleted_at"`
+	Bengkel     []Bengkel      `gorm:"foreignKey:MitraID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"bengkel"`
 }
