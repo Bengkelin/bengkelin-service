628ec2d3b20b6b734e2f5a5301ac9e23ae618f97
Muh Zaqi <zaqiikhwan22@gmail.com>
2024-07-28 11:51:07 +0700

    adjust: name for validator to be more consistent

--- internal/pkg/validator/validator.bengkel.go
+++ internal/pkg/validator/validator.bengkel.go
@@ -1,6 +1,6 @@
 package validator
 
-type RegisterBengkelRequest struct {
+type BengkelRegisterRequest struct {
 	BengkelName  string   `json:"bengkel_name"`
 	BengkelPhone string   `json:"bengkel_phone"`
 	JumlahMontir uint     `json:"jumlah_montir"`
@@ -8,10 +8,14 @@ type RegisterBengkelRequest struct {
 	JamBuka      []string `json:"jam_buka"`
 }
 
-type AddressMitraRequest struct {
+type BengkelAddressRequest struct {
 	Latitude     float64 `json:"latitude" binding:"required"`
 	Longitude    float64 `json:"longitude" binding:"required"`
 	AddressLabel string  `json:"address_label" binding:"required"`
 	FullAddress  string  `json:"full_address" binding:"required"`
 	Note         string  `json:"note"`
+}
+
+type BengkelServiceRequest struct {
+	NamaService []string `json:"nama_service"`
 }
