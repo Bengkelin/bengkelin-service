b7b6dcec223d62a0340590e099e79095ad1bddae
Muh Zaqi <zaqiikhwan22@gmail.com>
2024-07-28 11:50:21 +0700

    adjust: add id in bengkel.photo model

--- internal/pkg/models/bengkel.photo.go
+++ internal/pkg/models/bengkel.photo.go
@@ -1,7 +1,8 @@
 package models
 
 type BengkelPhoto struct {
-	BengkelID string  `gorm:"primary_key;reference;type:varchar(36)" json:"bengkel_id"`
+	ID        uint    `gorm:"primary_key;auto_increment" json:"photo_id"`
+	BengkelID string  `gorm:"primary_key;reference;type:varchar(36)" json:"-"`
 	PhotoURL  string  `gorm:"type:varchar(255)" json:"photo_url"`
 	Bengkel   Bengkel `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
 }
