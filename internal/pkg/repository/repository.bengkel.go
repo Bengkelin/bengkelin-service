5579439396218ad19c38f996a3e1c1fccafd83c0
Muh Zaqi <zaqiikhwan22@gmail.com>
2024-07-29 16:24:57 +0700

    feat: seeder for bengkel

--- internal/pkg/repository/repository.bengkel.go
+++ internal/pkg/repository/repository.bengkel.go
@@ -13,6 +13,8 @@ type BengkelRepositoryInterface interface {
 	CreateBengkel(bengkel models.Bengkel) (models.Bengkel, error)
 	UpdateBengkelById(bengkelId string, bengkel *models.Bengkel) error
 	GetBengkelById(bengkelId string) (*models.Bengkel, error)
+	GetAllBengkel() ([]models.Bengkel, error)
+	GetAllBengkelPaginate(page int, limit int) ([]models.Bengkel, int, error)
 }
 
 type BengkelRepository struct{}
@@ -54,4 +56,29 @@ func (*BengkelRepository) GetBengkelById(bengkelId string) (*models.Bengkel, err
 		return nil, err
 	}
 	return &bengkel, nil
+}
+
+// GetAllBengkel implements BengkelRepositoryInterface.
+func (*BengkelRepository) GetAllBengkel() ([]models.Bengkel, error) {
+	var bengkels []models.Bengkel
+	err := Find(models.Bengkel{}, &bengkels, []string{"Photos", "Addresses", "Operasionals"})
+	if err != nil {
+		return nil, err
+	}
+	return bengkels, nil
+}
+
+// GetAllBengkelPaginate implements BengkelRepositoryInterface.
+func (*BengkelRepository) GetAllBengkelPaginate(page int, limit int) ([]models.Bengkel, int, error) {
+	var bengkels []models.Bengkel
+	var count int64
+	err := db.GetDB().Model(&models.Bengkel{}).Count(&count).Error
+	if err != nil {
+		return nil, 0, err
+	}
+	err = db.GetDB().Preload("Photos").Preload("Services").Preload("Addresses").Preload("Operasionals").Offset((page - 1) * limit).Limit(limit).Find(&bengkels).Error
+	if err != nil {
+		return nil, 0, err
+	}
+	return bengkels, int(count), nil
 }
