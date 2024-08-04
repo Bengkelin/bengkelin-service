400dd79203cd94d2b0cbfac744d8019a0523cf9b
Muh Zaqi <zaqiikhwan22@gmail.com>
2024-08-04 16:37:29 +0700

    adjust: pesanan models

--- internal/pkg/models/pesanan.go
+++ internal/pkg/models/pesanan.go
@@ -6,12 +6,11 @@ type Pesanan struct {
 	ID             string           `gorm:"primary_key;type:varchar(36)" json:"id"`
 	UserID         string           `gorm:"type:varchar(36)" json:"user_id"`
 	BengkelID      string           `gorm:"type:varchar(36)" json:"bengkel_id"`
-	VehicleID      string           `gorm:"type:varchar(36)" json:"vehicle_id"`
+	VehicleID      uint             `gorm:"type:int" json:"vehicle_id"`
 	Status         uint             `gorm:"type:int" json:"status"`
-	IsHomeService  bool             `gorm:"type:bool" json:"is_home_service"`
+	IsHomeService  *bool            `gorm:"type:bool" json:"is_home_service"`
 	TotalPrice     float64          `gorm:"type:float" json:"total_price"`
-	HandlingFee    float64          `gorm:"type:float" json:"handling_fee"`
-	ServiceFee     float64          `gorm:"type:float" json:"service_fee"`
+	AdminFee       float64          `gorm:"type:float" json:"admin_fee"`
 	HomeServiceFee float64          `gorm:"type:float" json:"home_service_fee"`
 	Note           string           `gorm:"type:text" json:"note"`
 	User           User             `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"user"`
