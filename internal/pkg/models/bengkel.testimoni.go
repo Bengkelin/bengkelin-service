68191b0adfce3c20ca55ae2a4906b75e6c4e6c9f
Muh Zaqi <zaqiikhwan22@gmail.com>
2024-08-10 15:28:37 +0700

    adjust: add pesananId field in bengkel.testimoni models

--- internal/pkg/models/bengkel.testimoni.go
+++ internal/pkg/models/bengkel.testimoni.go
@@ -3,9 +3,11 @@ package models
 type BengkelTestimoni struct {
 	ID        uint    `gorm:"primary_key;autoIncrement" json:"id"`
 	BengkelID string  `gorm:"primary_key;type:varchar(36)" json:"-"`
+	PesananID string  `gorm:"type:varchar(36)" json:"pesanan_id"`
 	UserID    string  `gorm:"reference;type:varchar(36)" json:"-"`
 	Testimoni string  `gorm:"type:text" json:"testimoni"`
 	Rating    int     `gorm:"type:int" json:"rating"`
 	User      User    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"user"`
 	Bengkel   Bengkel `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
+	Pesanan   Pesanan `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
 }
