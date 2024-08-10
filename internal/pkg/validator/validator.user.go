cd9dbcf9c83fa116eeabdb6a1affc7d6b7d2049a
Muh Zaqi <zaqiikhwan22@gmail.com>
2024-08-10 16:26:41 +0700

    add: first_name & last_name field in update profile struct

--- internal/pkg/validator/validator.user.go
+++ internal/pkg/validator/validator.user.go
@@ -1,6 +1,8 @@
 package validator
 
 type UserUpdateRequest struct {
+	FirstName   string  `json:"first_name"`
+	LastName    string  `json:"last_name"`
 	PhoneNumber string  `json:"phone_number"`
 	Latitude    float64 `json:"latitude"`
 	Longitude   float64 `json:"longitude"`
