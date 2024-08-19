b80153619db2285ecd7255eee0e1ecd9cc758b2b
Muh Zaqi <zaqiikhwan22@gmail.com>
2024-08-19 18:01:41 +0700

    feat: MitraUpdateProfileRequest object for validator

--- internal/pkg/validator/validator.mitra.go
+++ internal/pkg/validator/validator.mitra.go
@@ -1,6 +1,12 @@
 package validator
 
-type MitraBankRequest struct {
+type MitraBankUpdateRequest struct {
 	BankName   string `binding:"required" json:"bank_name"`
 	BankNumber string `binding:"required" json:"bank_number"`
+}
+
+type MitraUpdateProfileRequest struct {
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	PhoneNumber string `json:"phone_number"`
 }
