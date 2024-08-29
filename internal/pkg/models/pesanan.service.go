112b19cd90307156ac0fdddf284b215ae80ac74a
Muh Zaqi <zaqiikhwan22@gmail.com>
2024-08-29 16:11:44 +0700

    adjust: some field in PesananService object

--- internal/pkg/models/pesanan.service.go
+++ internal/pkg/models/pesanan.service.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type PesananService struct {
-	ID          uint      `gorm:"primary_key;autoIncrement" json:"id"`
-	PesananID   string    `gorm:"primary_key;reference;type:varchar(36)" json:"pesanan_id"`
-	ServiceName string    `gorm:"type:varchar(255)" json:"service_name"`
-	Note        string    `gorm:"type:varchar(255)" json:"note"`
-	Price       float64   `gorm:"type:float" json:"price"`
-	Pesanan     Pesanan   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID        uint      `gorm:"primary_key;autoIncrement" json:"id"`
+	PesananID string    `gorm:"primary_key;reference;type:varchar(36)" json:"pesanan_id"`
+	Title     string    `gorm:"type:varchar(255)" json:"title"`
+	Detail    string    `gorm:"type:varchar(255)" json:"detail"`
+	Price     float64   `gorm:"type:float" json:"price"`
+	Pesanan   Pesanan   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
