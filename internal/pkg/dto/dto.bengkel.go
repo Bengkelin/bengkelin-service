16ca54232f811b529097e1229182989cd6895f1e
Muh Zaqi <zaqiikhwan22@gmail.com>
2024-09-04 19:39:17 +0700

    adjust: make some field is nullable

--- internal/pkg/dto/dto.bengkel.go
+++ internal/pkg/dto/dto.bengkel.go
@@ -5,10 +5,10 @@ import (
 )
 
 type BengkelDto struct {
-	ID           string                      `json:"bengkel_id"`
-	BengkelName  string                      `json:"bengkel_name"`
-	BengkelPhoto string                      `json:"bengkel_photo"`
-	Address      models.BengkelAddress       `json:"address"`
-	Distance     float64                     `json:"distance"`
-	Operasionals []models.BengkelOperasional `json:"operasionals"`
+	ID           string                       `json:"bengkel_id"`
+	BengkelName  string                       `json:"bengkel_name"`
+	BengkelPhoto *string                      `json:"bengkel_photo"`
+	Address      *models.BengkelAddress       `json:"address"`
+	Distance     *float64                     `json:"distance"`
+	Operasionals *[]models.BengkelOperasional `json:"operasionals"`
 }
